81996036d19246e4bd9ebcc4f6e67811217beee7
Alisonj37 <133525620+Alisonj37@users.noreply.github.com>
2023-09-26 22:53:05 -0300

    calculadora site de HTML , CSS , JAVASCRIPT

---
+++ index.js
@@ -0,0 +1,66 @@
+function addition(){
+   let n1= document.getElementById("number1").value
+   let n2= document.getElementById("number").value
+
+   let number1 = parseFloat(n1);
+   let number = parseFloat(n2);
+
+   let addition = number1 + number;
+   window.alert("o resultado da adição: " + addition)
+
+}
+
+function multiplicar(){
+   let n1= document.getElementById("number1").value
+   let n2= document.getElementById("number").value
+
+   let number1 = parseFloat(n1);
+   let number = parseFloat(n2);
+
+   let multiplicar = number1 * number;
+   window.alert("o resultado da multiplicação: " + multiplicar)
+}
+
+function subtração(){
+   let n1= document.getElementById("number1").value
+   let n2= document.getElementById("number").value
+
+   let number1 = parseFloat(n1);
+   let number = parseFloat(n2);
+
+   let subtração = number1 - number;
+   window.alert("o resultado da subtração: " + subtração)
+}
+
+function divisão(){
+   let n1= document.getElementById("number1").value
+   let n2= document.getElementById("number").value
+
+   let number1 = parseFloat(n1);
+   let number = parseFloat(n2);
+
+   let divisão = number1 / number;
+   window.alert("o resultado da divisão: " + divisão)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
